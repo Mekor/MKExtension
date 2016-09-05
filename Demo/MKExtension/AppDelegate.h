9c8bc0a4f60f26d4ec6f58edf7c79d1268fe1ca3
LiXiaozheng <mekor@live.cn>
2016-09-05 13:47:10 +0800

    初始化项目

---
+++ Demo/MKExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKExtension
+//
+//  Created by Mekor on 16/9/5.
+//  Copyright © 2016年 李小争. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
